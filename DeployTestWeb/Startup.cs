939e494188e847c66162dd787d40dcf0467fda1e
oleg <oleg@albekov.ru>
2016-12-21 00:07:12 +0300

    hello world

---
+++ DeployTestWeb/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+
+namespace DeployTestWeb
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.Run(async ctx => await ctx.Response.WriteAsync("Hello World!"));
+        }
+    }
+}
